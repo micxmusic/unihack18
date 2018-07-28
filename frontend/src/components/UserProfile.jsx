a4451d31ed5e3efce2a320bf37fe916dd528f10a
Daryl Wong <azureguard@users.noreply.github.com>
2018-07-29 09:17:54 +1000

    User profile page redesign

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,31 +1,63 @@
 import React, { Component } from "react";
-import { Media, Container, Row, Col, Button } from "reactstrap";
+import { Form, Table, Media, Container, Row, Col, Button } from "reactstrap";
 
 class UserProfile extends Component {
   render() {
     return (
       <React.Fragment>
-        <Media>
-          <Media left href="#">
-            <Media object data-src="holder.js/64x64" alt="User Profile pic" />
-          </Media>
-          <Media body>
-            <Container>
-              <Row>
-                <Media heading>Username: Unihack18</Media>
-              </Row>
-              <Row>
-                <Media heading>Password: XXXXXXXX</Media>
-                <Button color="link">Change password</Button>
-              </Row>
-              <Row>Equiptment owned:</Row>
-              <Row>Stove</Row>
-              <Row>Microwave</Row>
-              <Row>Dietry Restrictions:</Row>
-              <Row>None</Row>
-            </Container>
-          </Media>
-        </Media>
+        <Container className="mw-40">
+          <Col>
+            <Row>
+              <Media left href="#">
+                <Media
+                  object
+                  data-src="holder.js/64x64"
+                  alt="User Profile pic"
+                />
+              </Media>
+            </Row>
+            <Table>
+              <tbody>
+                <tr>
+                  <td>
+                    <b>Username</b>
+                  </td>
+                  <td>Unihack18</td>
+                  <td />
+                </tr>
+                <tr>
+                  <td>
+                    <b>Password</b>
+                  </td>
+                  <td>
+                    <Form>*********</Form>
+                  </td>
+                  <td>
+                    <Button className="m-0 p-0" color="link">
+                      Change password
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+            <h4>Equipment Owned</h4>
+            <Table>
+              <tbody>
+                <tr>
+                  <td>
+                    Stove <br /> Microwave
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+            <h4>Dietary Restrctions</h4>
+            <Table>
+              <tr>
+                <td>None</td>
+              </tr>
+            </Table>
+          </Col>
+        </Container>
       </React.Fragment>
     );
   }
